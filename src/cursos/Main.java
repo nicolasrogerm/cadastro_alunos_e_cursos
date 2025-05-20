871eb81816dbdd3f403a8ee67a6c6e9e897023e9
Nicolas Roger <nramonteirods05@gmail.com>
2025-05-20 20:09:00 -0300

    Adjust console output display order on Main.java

--- src/cursos/Main.java
+++ src/cursos/Main.java
@@ -5,18 +5,18 @@ public class Main {
 	public static void main(String[] args) {
 
 		Aluno aluno1 = new Aluno("Nicolas", "01", 19);
-		Curso curso1 = new Curso("DS", "93314", 500);
-		
 		Aluno aluno2 = new Aluno("Isa", "02", 18);
+		
+		Curso curso1 = new Curso("DS", "93314", 500);
 		Curso curso2 = new Curso("ADM", "94415", 400);
 		
 		Matricula matricula1 = new Matricula(2024, 2, aluno1.getNome(), curso1.getNome());
 		Matricula matricula2 = new Matricula(2025, 1, aluno2.getNome(), curso2.getNome());
 		
 		aluno1.exibirInformacoesAluno1(aluno1);
-		aluno2.exibirInformacoesAluno1(aluno2);
-		
 		curso1.exibirInformacoesCurso1(curso1);
+		
+		aluno2.exibirInformacoesAluno1(aluno2);
 		curso2.exibirInformacoesCurso2(curso2);
 		
 		matricula1.exibirInformacoesDaMatricula(aluno1, curso1, matricula1);
