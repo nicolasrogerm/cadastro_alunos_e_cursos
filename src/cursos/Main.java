bc3e680fb78a0284211c1c6ddb3bca184a137d66
Nicolas Roger <nramonteirods05@gmail.com>
2025-05-20 20:04:34 -0300

    Modifying Main.java

--- src/cursos/Main.java
+++ src/cursos/Main.java
@@ -5,9 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 
 		Aluno aluno1 = new Aluno("Nicolas", "01", 19);
-		Aluno aluno2 = new Aluno("Isa", "02", 18);
-		
 		Curso curso1 = new Curso("DS", "93314", 500);
+		
+		Aluno aluno2 = new Aluno("Isa", "02", 18);
 		Curso curso2 = new Curso("ADM", "94415", 400);
 		
 		Matricula matricula1 = new Matricula(2024, 2, aluno1.getNome(), curso1.getNome());
