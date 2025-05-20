c74136e3ec34954d828a5961e10753f1946e4f74
Nicolas Roger <nramonteirods05@gmail.com>
2025-05-20 19:19:18 -0300

    creating objects and showing all results on console

--- src/cursos/Matricula.java
+++ src/cursos/Matricula.java
@@ -4,18 +4,17 @@ public class Matricula {
 	
 	private int ano;
 	private int semestre;
-	private Aluno aluno;
-	private Curso curso;
+	private String aluno ;
+	private String curso;
 	
 	
-	public Matricula(int ano, int semestre, Aluno aluno, Curso curso) {
+	public Matricula(int ano, int semestre, String aluno, String curso) {
 		super();
 		this.ano = ano;
 		this.semestre = semestre;
 		this.aluno = aluno;
 		this.curso = curso;
 	}
-
 
 	public int getAno() {
 		return ano;
@@ -37,12 +36,12 @@ public class Matricula {
 	}
 
 
-	public Aluno getAluno() {
+	public String getAluno() {
 		return aluno;
 	}
 
 
-	public void setAluno(Aluno aluno) {
+	public void setAluno(String aluno) {
 		this.aluno = aluno;
 	}
 
@@ -52,16 +51,17 @@ public class Matricula {
 	}
 
 
-	public void setCurso(Curso curso) {
+	public void setCurso(String curso) {
 		this.curso = curso;
 	}
 	
-	public void exibirInformacoesDaMatricula1(Aluno aluno1, Curso curso1, Matricula matricula1) {
+	public void exibirInformacoesDaMatricula(Aluno aluno1, Curso curso1, Matricula matricula1) {
 		System.out.println("Nome do Aluno(a): " + aluno1.getNome());
 		System.out.println("Nome do curso: " + curso1.getNome());
 		System.out.println("Carga horária total: " + curso1.getCargaHoraria());
 		System.out.println("Ano: " + matricula1.getAno());
 		System.out.println("Semestre: " + matricula1.getSemestre());
+		System.out.println();
 	}
 	
 	
